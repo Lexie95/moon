4bb8a94894b8010114c90ef65085a630e3365503
Lexie95 <32326197+Lexie95@users.noreply.github.com>
2017-10-05 11:20:40 +0200

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,23 @@
+function setup() {
+  createCanvas(750,1334);
+  background(200);
+  angleMode(DEGREES);
+  
+  frameRate(20);
+}
+
+function draw() {
+  translate(width/2,height/2);
+  rotate(frameCount*6);
+  noStroke();
+
+  // lerpColor blends two colors to find a third color somewhere between them.
+  // lerpColor(col1,col2,amount)
+  
+  fill(lerpColor(color(92,0,26,10), color(6,94,130,10), frameCount/200));
+  rect(100,0,50+frameCount*6,50+frameCount*6);
+  
+  //if (frameCount == 240) {
+  //noLoop();
+  //}
+}
